23ea86d328aef256890a7a3017a2d24cca1e23ae
pangmingyu <pangmingyu@richstonedt.com>
2018-01-19 13:40:02 +0800

    draw_from_nabData

---
+++ draw_from_nabData.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 11:40:34 2018
+
+@author: pangmingyu
+"""
+
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+dir = 'D:\RichStone_Work\data'
+list = os.listdir(dir)
+k=58
+df=[]
+for i in range(k):
+    df.append([])
+i=0
+for line in list:
+    path = os.path.join(dir,line)
+    if os.path.isdir(path):
+        for li in os.listdir(path):
+            li = os.path.join(path,li)
+            if os.path.isfile(li):
+                file = pd.read_csv(li)
+                df[i] = file
+                df[i].columns = ['timestamp',li]
+                plt.figure() 
+                print(df[i].iloc[:,1].plot(subplots=False))
+                plt.legend()
+                ax = df[i].plot() 
+                fig = ax.get_figure()
+                fig.savefig(li[:-4]+'.png')
+                i+=1
+                #break
+    elif os.path.isfile(path):
+        pass
+    #break
+print(k)
+
+            
+
+
